7cb0e8097ed1f7cdbaebe430b4f0c90550825c28
Nokome Bentley <nokome@stenci.la>
2019-09-24 14:21:34 +1200

    feat(Dev shell): Add devug option to shell

--- host/shell.ts
+++ host/shell.ts
@@ -1,22 +1,62 @@
+/**
+ * A simple shell for testing connection to, and configuration of,
+ * guest VMs. Defaults to using `sh`.
+ * 
+ * Usage:
+ * 
+ * ```bash
+ * npm run dev:shell
+ * ```
+ * 
+ * For full debug level log messages use:
+ * 
+ * ```bash
+ * npm run dev:shell:debug
+ * ```
+ * 
+ * Sends requests to execute a `CodeChunk` with `programmingLanguage: 'sh'`
+ * to the VM and prints it's `outputs` to the console.
+ */
+
+import { LogLevel, replaceHandlers } from '@stencila/logga';
 import * as readline from 'readline'
 import Machine from './Machine'
 
 const red = '\u001b[31;1m'
 const blue = '\u001b[34;1m'
+const grey = '\u001b[30;1m'
 const reset = '\u001b[0m'
 
-;(async () => {
-  const machine = new Machine()
+// Collect options from command line
+let debug = false
+for (const arg of process.argv.slice(2)) {
+  if (arg === '--debug') debug = true
+}
 
+/** 
+ * Configure log handler to only show `info` and `debug` events
+ * when `--debug` flag is set
+ */
+replaceHandlers(data => {
+  const { level, tag, message } = data
+  if (level <= (debug ? LogLevel.debug : LogLevel.warn)) {
+    process.stderr.write(`${grey}${tag}: ${LogLevel[level].toUpperCase()}: ${message}${reset}\n`)
+  }
+})
+
+;(async () => {
+  // Create and start a new VM
+  const machine = new Machine()
+  await machine.start()
+
+  // Create the REPL with the starting prompt
   const repl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   })
-
-  await machine.start()
-
   repl.prompt()
 
+  // For each line the user enters...
   repl.on('line', async (line: string) => {
     // When user enters a line, execute a shell `CodeChunk`
     const result = await machine.call('execute', {
