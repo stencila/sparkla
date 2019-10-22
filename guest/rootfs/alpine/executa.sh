4fa18e81698d94f129f28a64a84508f3ebd1031c
Ben Shaw <ben@beneboy.co.nz>
2019-10-22 14:52:32 +1300

    feat: Updated alpine docker image to use executa

---
+++ guest/rootfs/alpine/executa.sh
@@ -0,0 +1,4 @@
+#!/sbin/openrc-run
+  
+command="/usr/bin/node"
+command_args="node_modules/@stencila/executa/dist/lib/cli.js --stdio"
--- guest/rootfs/alpine/execution-server.sh
+++
@@ -1,4 +0,0 @@
-#!/sbin/openrc-run
-  
-command="/usr/bin/node"
-command_args="/usr/bin/execution-server"
