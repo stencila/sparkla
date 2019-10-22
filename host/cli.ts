8ab168359e2de38b67cac3fdc250b06ab00a4a2e
Nokome Bentley <nokome@stenci.la>
2019-10-22 15:45:43 +1300

    feat(CLI): Rename serve.ts to cli.ts and make bin command

--- host/cli.ts
+++ host/cli.ts
@@ -1,25 +1,32 @@
+#!/usr/bin/env node
+
 /**
- * Serve the `Manager`.
+ * Command line interface to serve the `Manager`.
  *
  * Usage examples:
  *
- *    JWT_SECRET=not-a-secret npx ts-node-dev host/serve --debug --docker
- *    JWT_SECRET=not-a-secret npx ts-node host/serve
- *    JWT_SECRET=not-a-secret node dist/host/serve
+ *    JWT_SECRET=not-a-secret npx ts-node-dev host/cli --debug --docker
+ *    JWT_SECRET=not-a-secret npx ts-node host/cli
+ *    JWT_SECRET=not-a-secret node dist/host/cli
  */
 
-import { defaultHandler, LogLevel, replaceHandlers } from '@stencila/logga'
+import { defaultHandler, LogLevel, replaceHandlers, getLogger } from '@stencila/logga'
 import { Manager, SessionType } from './Manager'
 import { FirecrackerSession } from './FirecrackerSession'
 import { DockerSession } from './DockerSession'
+
+const log = getLogger('sparkla:cli')
 
 // Collect options from command line
 let debug = false
 let sessionType: SessionType = FirecrackerSession
 for (const arg of process.argv.slice(2)) {
   if (arg === '--debug') debug = true
-  if (arg === '--docker') sessionType = DockerSession
-  if (arg === '--firecracker') sessionType = FirecrackerSession
+  else if (arg === '--docker') sessionType = DockerSession
+  else if (arg === '--firecracker') sessionType = FirecrackerSession
+  else {
+    log.warn(`Unrecognised argument will be ignored: ${arg}`)
+  }
 }
 
 /**
