576557b283e4477bc30c2d49a2b82567558db601
Nokome Bentley <nokome@stenci.la>
2019-11-12 17:59:43 +1300

    fix(CLI): Add help command

--- host/cli.ts
+++ host/cli.ts
@@ -30,6 +30,7 @@ Usage:
 Commands:
   serve           Start the Sparkla web socket server (default)
   config          Show the configuration that has been loaded
+  help            Show this message
 
 Options:
   --debug         Output debug level log entries?
@@ -63,6 +64,7 @@ commands.map((command: string) => {
       return showConfig()
     case 'serve':
       return serve()
+    case 'help':
     default:
       console.error(usage)
   }
